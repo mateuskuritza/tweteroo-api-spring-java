456506e9d76bd902945b74e5715314b2075afc77
mateuskuritza <mateuskuritza@gmail.com>
2023-01-18 12:08:51 -0300

    feat: get tweets by username

--- src/main/java/com/api/tweteroo/controllers/TweetsController.java
+++ src/main/java/com/api/tweteroo/controllers/TweetsController.java
@@ -3,10 +3,12 @@ package com.api.tweteroo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +37,13 @@ public class TweetsController {
         Pageable pageable = PageRequest.of(page, 5);
         Page<Tweet> tweets = tweetRepository.findAll(pageable);
         return tweets.getContent();
+    }
+
+    @GetMapping("/{username}")
+    public List<Tweet> byUser(@PathVariable(value = "username") String username){
+        Tweet tweet = new Tweet();
+        tweet.setUsername(username);
+        Example<Tweet> tweetExample = Example.of(tweet);
+        return tweetRepository.findAll(tweetExample);
     }
 }

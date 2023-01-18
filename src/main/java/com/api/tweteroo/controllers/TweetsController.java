392bcadfa496d428448b0c1924891621fd04e384
mateuskuritza <mateuskuritza@gmail.com>
2023-01-18 11:58:44 -0300

    feat: get tweets with pagination

--- src/main/java/com/api/tweteroo/controllers/TweetsController.java
+++ src/main/java/com/api/tweteroo/controllers/TweetsController.java
@@ -1,9 +1,16 @@
 package com.api.tweteroo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.tweteroo.dtos.TweetDTO;
@@ -21,5 +28,12 @@ public class TweetsController {
     public String create(@RequestBody TweetDTO tweet){
         tweetRepository.save(new Tweet(tweet));
         return "OK";
+    }
+
+    @GetMapping
+    public List<Tweet> list(@RequestParam(value = "page", defaultValue = "1") int page){
+        Pageable pageable = PageRequest.of(page, 5);
+        Page<Tweet> tweets = tweetRepository.findAll(pageable);
+        return tweets.getContent();
     }
 }
